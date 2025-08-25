d27edc42506ad7e3dc527b2c79882e61053f9878
Junior <junior_silvapereira@hotmail.com>
2025-08-25 00:19:29 -0300

    feat: first commit + initial config

---
+++ src/main.tsx
@@ -0,0 +1,30 @@
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+
+import { routeTree } from './routeTree.gen'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
+import { Toaster } from 'sonner'
+import './index.css'
+
+const router = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+const rootElement = document.getElementById('root')!
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <Toaster richColors />
+      </QueryClientProvider>
+    </StrictMode>,
+  )
+}
---
+++ src/routes/(auth)/sign-in.tsx
@@ -0,0 +1,95 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Button } from '@/components/ui/button'
+import { useMutation } from '@tanstack/react-query'
+import { createFileRoute } from '@tanstack/react-router'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import { z } from 'zod'
+import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
+
+const signInSchema = z.object({
+  email: z.string().optional(),
+})
+
+type SignInSchema = z.infer<typeof signInSchema>
+
+export const Route = createFileRoute('/(auth)/sign-in')({
+  validateSearch: signInSchema,
+  component: SignIn,
+})
+
+function SignIn() {
+  const { email } = Route.useSearch()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInSchema>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: email ?? '',
+    },
+  })
+
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: async () => {},
+  })
+
+  async function handleAuthenticate({ email }: SignInSchema) {
+    try {
+      await authenticate({ email })
+
+      toast.success('Enviamos um link de autenticação para seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => authenticate({ email }),
+        },
+      })
+    } catch (err) {
+      toast.error('Credenciais inválidas')
+    }
+  }
+
+  return (
+    <div className="lg:p-8">
+      <Button asChild variant={'ghost'}>
+        <a href="/sign-up">Novo estabelecimento</a>
+      </Button>
+
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="flex flex-col space-y-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Acessar painel
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Acompanhe suas vendas pelo painel do parceiro!
+          </p>
+        </div>
+
+        <div className="grid gap-6">
+          <form onSubmit={handleSubmit(handleAuthenticate)}>
+            <div className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Seu e-mail</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  autoCapitalize="none"
+                  autoComplete="email"
+                  autoCorrect="off"
+                  {...register('email')}
+                />
+              </div>
+
+              <Button type="submit" disabled={isSubmitting}>
+                Acessar painel
+              </Button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
