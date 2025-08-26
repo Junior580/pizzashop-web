d6433063c49aa34ae49ff2bd6916664f7834d0c5
Junior <junior_silvapereira@hotmail.com>
2025-08-26 13:48:41 -0300

    feat(__root.tsx): adicionado rota de notfound

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,12 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 
+import { NotFound } from '@/_404'
+
 export const Route = createRootRoute({
   component: () => (
     <>
       <Outlet />
     </>
   ),
+  notFoundComponent: NotFound,
 })
